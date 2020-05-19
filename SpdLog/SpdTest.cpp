0d717a808367db25f09adfac02b590e1422c75df
sw1024 <1106012118@qq.com>
2020-05-19 10:31:22 +0800

    task:00 修改spdlog 提交部分测试代码

--- SpdLog/SpdTest.cpp
+++ SpdLog/SpdTest.cpp
@@ -9,14 +9,68 @@
 #include "spdlog/spdlog.h"
 #include "spdlog/common.h"
 #include "spdlog/cfg/env.h"
-#include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/sinks/hour_file_sink.h"
+#include "spdlog/async.h"
+#include "spdlog/async_logger.h"
 
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+#include "unistd.h"
+
+using spdlogType = std::shared_ptr<spdlog::async_logger>;
+
+static spdlogType aslog = nullptr;
+
+void ThreadPrint(int i)
+{
+	auto tlog = aslog;
+	std::cout<<"spdth:"<<i<<" "<<tlog.use_count()<<std::endl;
+	for (int i = 0;i < 100; i++){
+		tlog->info(" hello this is spdlog {}", i);
+	}
+	//tlog->flush();
+	sleep(1);
+}
 int main()
 {
-	auto daily_logger = spdlog::daily_logger_mt("daily_logger", "./daily.log", 2, 30);
-	daily_logger->set_level(spdlog::level::trace);
-	daily_logger->debug("debug hello this is spdlog");
-	daily_logger->info("info hello this is spdlog");
+	std::string filename = "./logs/testspd.log";
+	std::string logname = "SunWu";
+	//定义输出对象
+	std::vector<spdlog::sink_ptr> sink_vec;
+	sink_vec.push_back(std::make_shared<spdlog::sinks::hour_file_sink_mt>(filename));
+	//sink_vec.push_back(std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>());
+	//创建异步写入log
+	auto tp = std::make_shared<spdlog::details::thread_pool>(1024,1);
+	auto hour_logger = std::make_shared<spdlog::async_logger>(logname, std::begin(sink_vec), std::end(sink_vec), tp);
+	//设置日志输出等级
+	hour_logger->set_level(spdlog::level::trace);
+	//设置日志格式
+	hour_logger->set_pattern("%^[%Y%m%d %H:%M:%S.%e][%l][%n]%v%$");
+	//注册到日志管理器中
+	spdlog::register_logger(hour_logger);
 
+
+	//hour_logger->debug("debug hello this is spdlog {}", i);
+	aslog = hour_logger;
+
+
+
+
+	std::vector<std::thread> th;
+	for (int i =0; i < 10; i++) {
+		std::cout<<"for:"<<i<<" "<<aslog.use_count()<<std::endl;
+		th.emplace_back(std::thread([&] { ThreadPrint(i); }));
+	}
+
+
+	for (auto& it : th) {
+		it.join();
+	}
+
+
+
+	spdlog::drop_all();
 	return 0;
 }
